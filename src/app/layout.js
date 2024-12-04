1a3e10bca6311112c0843766f611270b9a712e7d
kksladder <kksladder@gmail.com>
2024-12-04 17:38:38 +0900

    hyukoh

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ export default function RootLayout({ children }) {
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <title>HyukOh AAA</title>
                 <meta property='og:type' content='website' />
-                <meta property='og:title' content='페이지 제목' />
-                <meta property='og:description' content='페이지 설명' />
+                <meta property='og:title' content='혁오 밴드' />
+                <meta property='og:description' content='혁오 밴드 아카이브 입니다.' />
                 <meta property='og:image' content='http://www.mysite.com/article/article1_featured_image.jpg' />
                 <meta property='og:url' content='http://www.mysite.com/article/article1.html' />
                 <meta name='twitter:card' content='summary' />
