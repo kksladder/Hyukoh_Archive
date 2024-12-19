c4f860d3b1dbe461055aff699d89c457d4a51789
kksladder <kksladder@gmail.com>
2024-12-19 18:06:35 +0900

    로딩 화면 인터페이스 수정

--- src/app/home/home.jsx
+++ src/app/home/home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
                                 if (onLoadComplete) {
                                     onLoadComplete();
                                 }
-                            }, 8000);
+                            }, 10000);
                         },
                     },
                 });
